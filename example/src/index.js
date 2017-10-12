d690e9d4ee060290084ee9ee64aa01a12e308cda
Jungle <devex.soft@gmail.com>
2017-10-12 16:43:49 +0500

    Fix import path element-port.js

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,6 @@
 import './main.css';
 import { Main } from './Main.elm';
-import { init } from './CustomSearch/element-port.js';
+import { init } from '../../src/CustomSearch/element-port.js';
 
 const app = Main.embed(document.getElementById('root'));
 init(app);
